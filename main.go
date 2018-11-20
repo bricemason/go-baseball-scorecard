8347b5985b53e9e4ddf42f8383614a48e0ab6cbf
Brice Mason <bricemason@gmail.com>
2018-11-19 19:55:30 -0500

    Reading in Event Files - read in event file, processing line by line - detect record type, split into fields - added 1986 Mets home game play-by-play file to use for testing

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func main() {
-	fmt.Println("go baseball scorecard")
+	// hard-coding in 1986 Mets home games for now
+	eventData, err := ioutil.ReadFile("data/1986NYN.EVN")
+	if err != nil {
+		fmt.Println("Error reading event file", err)
+		return
+	}
+
+	// []string of full records in the file
+	records := strings.Split(string(eventData), "\n")
+
+	for _, record := range records {
+		// []string of fields in the record
+		fields := strings.Split(record, ",")
+
+		// the first item is the field type (id, info, play etc)
+		recordType := fields[0]
+
+		if recordType == "id" {
+			fmt.Println("Game:", fields[1])
+		}
+	}
 }
